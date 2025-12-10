15375b6de87421d7441346eae409dfc9b40e6863
chaksseu <rtrt505@korea.ac.kr>
2025-12-10 07:36:14 +0000

    update 1210 .125

--- train_rkd_x0_pred_with_teacherx0_inv_only_x0_no_norm_1209.py
+++ train_rkd_x0_pred_with_teacherx0_inv_only_x0_no_norm_1209.py
@@ -27,13 +27,13 @@ import itertools
 W_RKD = 0.08
 W_INV = 0.1
 W_INVINV = 1.0
-W_FID = 0.001
-W_SAME = 0.01
+W_FID = 0.1
+W_SAME = 0.1
 
 CUDA_NUM = 3
 BATCH_SIZE = 1024
 
-WANDB_NAME=f"1209_lr1e4_n32_b{BATCH_SIZE}_T100_ddim_30_50_steps_no_init_rkdW{W_RKD}_invW{W_INV}_invinvW{W_INVINV}_fidW{W_FID}_sameW{W_SAME}_x0_pred_rkd_with_teacher_x0_inv_only_x0_no_norm"
+WANDB_NAME=f"1210_lr1e4_n32_b{BATCH_SIZE}_T100_ddim_30_50_steps_no_init_rkdW{W_RKD}_invW{W_INV}_invinvW{W_INVINV}_fidW{W_FID}_sameW{W_SAME}_x0_pred_rkd_with_teacher_x0_inv_only_x0_no_norm"
 
 
 CONFIG = {
